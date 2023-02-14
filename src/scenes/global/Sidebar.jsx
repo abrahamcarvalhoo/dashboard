a7ddf91d0b1383e4d8c4474e98be695f382f1f31
abrahamcarvalhoo <abraham.carvalhoo@gmail.com>
2023-02-14 22:13:57 +0000

    Added Team and renamed js to jsx

--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -1,8 +1,8 @@
 import { useState } from 'react';
 import { ProSidebar, Menu, MenuItem } from 'react-pro-sidebar';
-import 'react-pro-sidebar/dist/css/styles.css';
 import { Box, IconButton, Typography, useTheme } from '@mui/material';
 import { Link } from 'react-router-dom';
+import 'react-pro-sidebar/dist/css/styles.css';
 import { tokens } from '../../theme';
 import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
 import PeopleOutlinedIcon from "@mui/icons-material/PeopleOutlined";
@@ -10,7 +10,7 @@ import ContactsOutlinedIcon from "@mui/icons-material/ContactsOutlined";
 import ReceiptOutlinedIcon from "@mui/icons-material/ReceiptOutlined";
 import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
 import CalendarTodayOutlinedIcon from "@mui/icons-material/CalendarTodayOutlined";
-import HelpOutlinedIcon from "@mui/icons-material/HelpOutlined";
+import HelpOutlineOutlinedIcon from "@mui/icons-material/HelpOutlineOutlined";
 import BarChartOutlinedIcon from "@mui/icons-material/BarChartOutlined";
 import PieChartOutlineOutlinedIcon from "@mui/icons-material/PieChartOutlineOutlined";
 import TimelineOutlinedIcon from "@mui/icons-material/TimelineOutlined";
@@ -21,7 +21,7 @@ const Item = ({ title, to, icon, selected, setSelected }) => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
   return (
-    <MenuItem active={selected === title} style={{color: colors.grey[100]}} onCLick={() => setSelected(title)} icon={icon}>
+    <MenuItem active={selected === title} style={{ color: colors.grey[100] }} onClick={() => setSelected(title)} icon={icon}>
       <Typography>{title}</Typography>
       <Link to={to} />
     </MenuItem>
@@ -57,10 +57,7 @@ const Sidebar = () => {
           <MenuItem
             onClick={() => setIsCollapsed(!isCollapsed)}
             icon={isCollapsed ? <MenuOutlinedIcon /> : undefined}
-            style={{
-              margin: "10px 0 20px 0",
-              color: colors.grey[100]
-            }}>
+            style={{ margin: "10px 0 20px 0", color: colors.grey[100] }}>
             {!isCollapsed && (
               <Box display="flex" justifyContent="space-between" alignItems="center">
                 <Typography variant="h4" color={colors.grey[100]} textTransform="uppercase">React Admin</Typography>
@@ -133,7 +130,7 @@ const Sidebar = () => {
             <Item
             title="FAQ Page"
             to="/faq"
-            icon={<HelpOutlinedIcon />}
+            icon={<HelpOutlineOutlinedIcon />}
             selected={selected}
             setSelected={setSelected}
             />

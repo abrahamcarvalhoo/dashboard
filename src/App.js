48eb19573e7a058bccbbbe799b1d21afecbb12cc
abrahamcarvalhoo <abraham.carvalhoo@gmail.com>
2023-02-14 23:04:39 +0000

    Added Invoices

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Sidebar from "./scenes/global/Sidebar";
 import Dashboard from "./scenes/dashboard";
 import Team from "./scenes/team";
 import Contacts from "./scenes/contacts";
-// import Invoices from "./scenes/invoices";
+import Invoices from "./scenes/invoices";
 // import Form from "./scenes/form";
 // import Bar from "./scenes/bar";
 // import Pie from "./scenes/pie";
@@ -30,7 +30,7 @@ function App() {
             <Route path="/" element={<Dashboard />} />
             <Route path="/team" element={<Team />} />
             <Route path="/contacts" element={<Contacts />} />
-            {/* <Route path="/invoices" element={<Invoices />} /> */}
+            <Route path="/invoices" element={<Invoices />} />
             {/* <Route path="/form" element={<Form />} /> */}
             {/* <Route path="/bar" element={<Bar />} /> */}
             {/* <Route path="/pie" element={<Pie />} /> */}

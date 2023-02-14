66f1556a08f9e2b6a1b9f9803db1b66014378442
abrahamcarvalhoo <abraham.carvalhoo@gmail.com>
2023-02-14 00:42:39 +0000

    First Commit

---
+++ src/scenes/global/Topbar.js
@@ -0,0 +1,18 @@
+import { Box, IconButton, InputBase, useTheme } from "@mui/material";
+import { useContext } from "react";
+import { ColorModeContext, tokens } from "../../theme";
+import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
+import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import SearchIcon from "@mui/icons-material/Search";
+
+const Topbar = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+
+  return <div>Topbar</div>
+}
+
+export default Topbar;
